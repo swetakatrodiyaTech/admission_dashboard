40ab003b1f1ca1250246ba6c9457fccb9d2a4f16
Sweta Katrodiya <swetakatrodiya.tech@gmail.com>
2026-03-14 19:00:13 +0530

    fix theming

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import UniversityTheme from "../theme/UniversityTheme";
+
 function Card({ title, value, bgColor }) {
+
+  // decide color based on value
+  const getValueColor = (num) => {
+    if (num > 1000) return UniversityTheme.danger;   // red
+    if (num > 500) return UniversityTheme.warning;   // orange
+    return UniversityTheme.textPrimary;              // default
+  };
 
   return (
     <div
@@ -16,7 +24,13 @@ function Card({ title, value, bgColor }) {
         {title}
       </h3>
 
-      <h1 style={{ color: UniversityTheme.textPrimary }}>
+      <h1
+        style={{
+          color: getValueColor(value),
+          fontSize: "32px",
+          margin: 0
+        }}
+      >
         {value}
       </h1>
 
